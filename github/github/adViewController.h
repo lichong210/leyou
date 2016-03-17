36043a649b5f5ba204f15d721f26934a01279204
lanou3g <1102203370@qq.com>
2016-03-17 09:51:31 +0800

    第二次提交

---
+++ github/github/adViewController.h
@@ -0,0 +1,13 @@
+//
+//  adViewController.h
+//  github
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 lichong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface adViewController : ViewController
+
+@end
