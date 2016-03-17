bf2e5d2ff0a0f329e20328748f620aa1ecfead7e
lanou3g <1102203370@qq.com>
2016-03-17 09:39:45 +0800

    第一次提交

---
+++ github/github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 lichong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by lanou3g on 16/3/17.
+//  Copyright © 2016年 lichong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
